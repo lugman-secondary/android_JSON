4decfbb0c94e0cb0d45f8519efee56644d828496
Lugman <lugmansito@gmail.com>
2018-02-02 10:25:15 +0100

    Ultimas Modificaciones

--- app/src/main/java/com/example/lugman/tareaasinc/MainActivity.java
+++ app/src/main/java/com/example/lugman/tareaasinc/MainActivity.java
@@ -7,8 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -42,26 +44,55 @@ import java.util.List;
 import static android.R.attr.name;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn;
-    ListView list;
+    Button btn,btn2;
+    ListView list,list2;
     Asincrona tarea;
+    EditText ed1,ed2,ed3,ed4,ed5,ed6,ed7;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         tarea = new Asincrona();
+        tarea = new Asincrona();
         tarea.execute();
         btn =  findViewById(R.id.newB);
+        btn2 =  findViewById(R.id.newC);
+
+        ed1 = findViewById(R.id.editText);
+        ed2 = findViewById(R.id.editText2);
+        ed3 = findViewById(R.id.editText3);
+        ed4 = findViewById(R.id.editText4);
+        ed5 = findViewById(R.id.editText5);
+        ed6 = findViewById(R.id.editText6);
+        ed7 = findViewById(R.id.editText7);
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Añadir aña =  new Añadir("LECHE");
+                Añadir aña =  new Añadir(ed1.getText().toString(),ed2.getText().toString(),ed3.getText().toString(),"a","");
+                aña.execute();
+                ed1.setText("");
+                ed2.setText("");
+                ed3.setText("");
+
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Añadir aña =  new Añadir(ed4.getText().toString(),ed5.getText().toString(),ed6.getText().toString(),"b",ed7.getText().toString());
+                aña.execute();
+            }
+        });
+        list =  findViewById(R.id.list);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Añadir aña =  new Añadir(ed4.getText().toString(),ed5.getText().toString(),ed6.getText().toString(),"2",String.valueOf(i));
                 aña.execute();
             }
         });
 
-        list =  findViewById(R.id.list);
     }
 
     private class Asincrona extends AsyncTask <Void, String, Void> {
@@ -134,6 +165,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(Void aVoid) {
+
             super.onPostExecute(aVoid);
             try {
                 jsonRealizar(MiJSON);
@@ -147,10 +179,15 @@ public class MainActivity extends AppCompatActivity {
 
     }
     private class Añadir  extends AsyncTask <String,String,Void>{
-        String Producto;
+        String Nombre,Apellidos,Edad,tipo,id;
         String MiJSON2;
-        Añadir(String aña){
-         Producto=aña;
+
+        public Añadir(String nombre, String apellidos, String edad,String tipo,String id) {
+            this.Nombre = nombre;
+            this.id = id;
+            this.Apellidos = apellidos;
+            this.Edad = edad;
+            this.tipo = tipo;
         }
 
         @Override
@@ -159,7 +196,13 @@ public class MainActivity extends AppCompatActivity {
             URL url= null;
             HttpURLConnection urlConnection = null;
             try {
-                url = new URL("http://www.carlossilla.com.es/android/insertar.php");
+                if (tipo.equals("a")){
+                    url = new URL("http://www.carlossilla.com.es/android/insertar.php");
+                }else if(tipo.equals("b")){
+                    url = new URL("http://www.carlossilla.com.es/android/update.php");
+                }else {
+                    url = new URL("http://www.carlossilla.com.es/android/delete.php");
+                }
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             }
@@ -186,15 +229,14 @@ public class MainActivity extends AppCompatActivity {
                 JSONObject jsonObj = new JSONObject();
                 //Añadimos el nombre, apellidos y email del usuario
                 try {
-                    jsonObj.put("user","juan");
+                    jsonObj.put("id",id);
+                    jsonObj.put("nombre",Nombre);
+                    jsonObj.put("apellidos", Apellidos);
+                    jsonObj.put("edad", Edad);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-                try {
-                    jsonObj.put("pass", "pepe");
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+
 
                 List  l = new LinkedList();
                 l.addAll(Arrays.asList(jsonObj));
@@ -203,11 +245,9 @@ public class MainActivity extends AppCompatActivity {
 
                 String pasar = jsonObj.toString();
                 String urlParameters = "json="+jsonString;
-                Log.d("ENVIAR",urlParameters);
+//                Log.d("ENVIAR",urlParameters);
                 writer.writeBytes(urlParameters);
-
                 writer.flush();
-
                 writer.close();
 
                 InputStream in = new BufferedInputStream(urlConnection.getInputStream());
@@ -231,44 +271,43 @@ public class MainActivity extends AppCompatActivity {
             } finally {
                 urlConnection.disconnect();
             }
-
-
             return null;
         }
-
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
+            Asincrona tarea2 = new Asincrona();
+            tarea2.execute();
 //            tarea.execute();
-            try {
-                Log.d("REPETIDO",MiJSON2);
-                jsonRealizar2(MiJSON2);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
+//            try {
+//                Log.d("REPETIDO",MiJSON2);
+////                jsonRealizar2(MiJSON2);
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
         }
-
     }
-
     private void jsonRealizar(String json) throws JSONException {
                         JSONArray  arr = new JSONArray(json);
                         JSONObject obj = arr.getJSONObject(1);
-                        String TXT = obj.getString("Nombre");
+
                         ArrayList<String> lista = new ArrayList<String>();
+                        ArrayList<String> lista2 = new ArrayList<String>();
                                 for (int i = 0; i< arr.length(); i++){
-                                    lista.add(arr.getJSONObject(i).getString("Nombre"));
+                                    lista.add(arr.getJSONObject(i).getString("nombre")+" (id:"+arr.getJSONObject(i).getString("id")+")");
+                                    lista2.add("id");
                                 }
+
                         ArrayAdapter<String> adapter =  new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,lista);
                         list.setAdapter(adapter);
 
-        Log.d("BUENA",json);
+//        Log.d("BUENA",json);
 
     }
-    private void jsonRealizar2(String json) throws JSONException {
-        TextView tv =  findViewById(R.id.textView);
-        tv.setText(json);
-    }
+//    private void jsonRealizar2(String json) throws JSONException {
+//        TextView tv =  findViewById(R.id.textView);
+//        tv.setText(json);
+//    }
     }
 
 
